a599d5d009d0ac07500812213680edb670244506
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2024-08-18 15:54:25 +0200

    cloudwatch logs initialized

--- iam/custom_iam_policies.tf
+++ iam/custom_iam_policies.tf
@@ -12,3 +12,24 @@ resource "aws_iam_role_policy" "dynamodb-lambda-policy" {
     ]
   })
 }
+
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
