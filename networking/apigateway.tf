d8c23771f99d2e3918f265055f5188a4b9bc4d99
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2024-08-18 13:29:33 +0200

    Return to AWS_PROXY integration

--- networking/apigateway.tf
+++ networking/apigateway.tf
@@ -6,7 +6,7 @@ resource "aws_apigatewayv2_api" "moloko-blog-api-gateway" {
 
 resource "aws_apigatewayv2_integration" "moloko-blog-apigateway-lambda-integration" {
   api_id           = aws_apigatewayv2_api.moloko-blog-api-gateway.id
-  integration_type = "HTTP"
+  integration_type = "AWS_PROXY"
 
   connection_type      = "INTERNET"
   description          = "Loko Blok's API_gateway-Lambda integration"
