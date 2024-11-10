3f5703a0b4d4f80dcde94af02f09a3de9337f127
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2024-11-10 21:27:46 +0200

    Remove global_secondary_index

--- database/dynamodb.tf
+++ database/dynamodb.tf
@@ -26,15 +26,15 @@ resource "aws_dynamodb_table" "blog-dynamodb-table" {
     enabled        = true
   }
 
-  global_secondary_index {
-    name               = "BlogTitleIndex"
-    hash_key           = "BlogTitle"
-    range_key          = "PublishedDate"
-    write_capacity     = 20
-    read_capacity      = 20
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["BlogId"]
-  }
+  # global_secondary_index {
+  #   name               = "BlogTitleIndex"
+  #   hash_key           = "BlogTitle"
+  #   range_key          = "PublishedDate"
+  #   write_capacity     = 20
+  #   read_capacity      = 20
+  #   projection_type    = "INCLUDE"
+  #   non_key_attributes = ["BlogId"]
+  # }
 
   point_in_time_recovery {
     enabled = true
